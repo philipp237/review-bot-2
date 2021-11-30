ce24e4261feb43051f7a0efcc4454f59bb4cb704
annenkov <annenkov@lanit.ru>
2021-11-30 19:57:36 +0300

    init without flyway on 8081 port

---
+++ src/main/java/dev/reviewbot2/ReviewBot2Application.java
@@ -0,0 +1,13 @@
+package dev.reviewbot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewBot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewBot2Application.class, args);
+    }
+
+}

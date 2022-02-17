6281990a900bda75ca9f83a7f77d6ef660f84b69
annenkov <annenkov@lanit.ru>
2022-02-17 20:15:08 +0300

    query for /my_tasks command was fixed

--- src/main/java/dev/reviewbot2/repository/TaskRepository.java
+++ src/main/java/dev/reviewbot2/repository/TaskRepository.java
@@ -11,13 +11,13 @@ import java.util.List;
 public interface TaskRepository extends JpaRepository<Task, Long> {
     Task getByUuid(String uuid);
 
-    @Query(value = "SELECT * FROM task t " +
+    @Query(value = "SELECT t.* FROM task t " +
         "LEFT JOIN review r on t.id = r.task_id " +
         "LEFT JOIN member_review mr on r.id = mr.review_id " +
         "WHERE r.review_stage = :reviewGroup " +
         "AND t.status NOT IN ('CLOSED', 'FORCE_CLOSED', 'APPROVED') " +
         "AND mr.reviewer_id = :id " +
-        "GROUP BY t.id, r.id",
+        "GROUP BY t.id",
         nativeQuery = true)
     List<Task> getTaskInMemberReview(long id, int reviewGroup);
 }
